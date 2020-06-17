c335edc6175948afe8e81754fea35571167ff8ab
Tristan Guest <tristanguest@gmail.com>
2020-06-17 11:58:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,15 +1,23 @@
 var array = []
 
-function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
+// function forLoop(array) {
+//   for (let i = 0; i < 25; i++) {
     
-    if (i === 1) {
-      array.push(`I am ${i} strange loop.`)
-    } else {
-      array.push(`I am ${i} strange loops.`)
-  }
+//     if (i === 1) {
+//       array.push(`I am ${i} strange loop.`)
+//     } else {
+//       array.push(`I am ${i} strange loops.`)
+//   }
   
-  return array
+//   return array
+// }
+
+for (let i = 0; i < 25; i++) {
+  
+  if (i === 1) {
+    array.push(`I am ${i} strange loop.`)
+  } else {
+    array.push(`I am ${i} strange loops.`)
 }
 
 console.log(array)
