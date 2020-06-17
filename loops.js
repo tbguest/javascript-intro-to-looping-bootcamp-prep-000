f0d1478cbde182704eb4c6956e774773aedd283b
Tristan Guest <tristanguest@gmail.com>
2020-06-17 12:20:49 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -32,9 +32,19 @@ function whileLoop(number) {
 console.log(whileLoop(10))
 
 
-function doWhileLoop(i) {
-  do 
+let i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+} 
+
+function doWhileLoop(num) {
+  do {
+    console.log("I run once regardless.")
+  } while (incrementVariable() < num)
 }
+
 
 
 ```javascript
