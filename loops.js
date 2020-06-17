1188758300b8bd90d8f2f1d55402a852271d0e44
Tristan Guest <tristanguest@gmail.com>
2020-06-17 12:07:08 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,6 +17,9 @@ for (let i = 0; i < 25; i++) {
   if (i === 1) {
     array.push(`I am ${i} strange loop.`)
     console.log(array)
+  } else if (i > 1) {
+    array.push(`I am ${i} strange loops.`)
+    console.log(array)
   }
 }
 
